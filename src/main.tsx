460cde1ee8eea7aa6358e35e024f462c579ffef6
Aditya Solanki <24adityasolanki24@gmail.com>
2026-03-21 15:19:51 +1100

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import ErrorBoundary from './components/ErrorBoundary'
 import App from './App'
@@ -12,5 +13,6 @@ root.render(
     <ErrorBoundary fullScreen>
       <App />
     </ErrorBoundary>
+    <Analytics />
   </React.StrictMode>,
 )
